0850a378ba2da80c1f8f834b751a6891f8708079
Baychik <ergeshovbaistan@gmail.com>
2024-03-14 21:51:24 +0600

    added class Bayastan

--- src/main/java/org/example/Boss.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Boss {
-    public static void main(String[] args) {
-        System.out.println("THIS IS HERO");
-    }
-}
--- src/main/java/org/example/Hero.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Hero {
-    public static void main(String[] args) {
-        System.out.println("THIS IS HERO");
-    }
-}
